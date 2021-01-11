423092fc3c9c4eb69372cbafa047c290b9d761f0
awaqar <azzumwaqar@gmail.com>
2021-01-11 23:36:51 +0000

    Code after migration

--- src/Menu.java
+++ src/Menu.java
@@ -5,9 +5,18 @@ class Menu{
     private ArrayList<Option> optionArrayList;
     private Scanner in;
 
-    Menu(ArrayList<Option> options){
-        this.optionArrayList = options;
+    Menu(){
+        this.optionArrayList = new ArrayList<>();
+
+
+        optionArrayList.add(new Item("Show Products"));
+        optionArrayList.add(new Item("Add a new Product"));
+        optionArrayList.add(new Item("Restock Products"));
+        optionArrayList.add(new Item("Exit"));
         in = new Scanner(System.in);
+
+        int option = displayMenu();
+        processOptionFromMainMenu(option);
     }
 
 
@@ -38,5 +47,32 @@ class Menu{
         }while (input<1 || input > optionArrayList.size());
         this.clear();
         return input;
+    }
+
+    private void processOptionFromMainMenu(int optionEntered){
+        VendingMachine vm = new VendingMachine();
+        //only focusing on case 1 for now until code clean
+        switch (optionEntered){
+
+            case 1: System.out.println("Current Products Available");
+
+                clear();
+                optionArrayList.addAll(vm.showProducts());
+                this.setMenuOptions(optionArrayList);
+                int optionSelected = displayMenu();
+
+                vm.purchaseProduct(vm.showProducts().get(optionSelected-1));
+
+                break;
+
+            case 2: vm.addProduct(new Product("Dummy",4.00,1));
+
+                System.out.println(ProductInventory.getProductInventory().get(ProductInventory.getProductInventory().size()-1).getName());break;
+
+            case 3:
+            case 4: System.exit(0);
+
+            default:System.out.println("default");
+        }
     }
 }
--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -9,24 +9,16 @@ public class VendingMachine {
 
     public VendingMachine() {
         in = new Scanner(System.in);
-        options = new ArrayList<>();
-
-        options.add(new Item("Show Products"));
-        options.add(new Item("Add a new Product"));
-        options.add(new Item("Restock Products"));
-        options.add(new Item("Exit"));
-
-        menu = new Menu(options);
-        int optionSelectedFromMain = menu.displayMenu();
-        processOptionFromMainMenu(optionSelectedFromMain);
+//        int optionSelectedFromMain = menu.displayMenu();
+//        processOptionFromMainMenu(optionSelectedFromMain);
     }
 
     //add a product
-    private void addProduct(Product product){
+    public void addProduct(Product product){
         ProductInventory.addProduct(product);
     }
 
-    private ArrayList<Product> showProducts(){
+    public ArrayList<Product> showProducts(){
         return ProductInventory.getProductInventory();
     }
 
@@ -34,7 +26,7 @@ public class VendingMachine {
         ProductInventory.removeProduct(i);
     }
 
-    private void purchaseProduct(Product p){
+    public void purchaseProduct(Product p){
         //this method implementation is not complete yet.
         if(p.getQuantity()>0){
             //purchase the product
@@ -50,30 +42,30 @@ public class VendingMachine {
     }
 
 
-    private void processOptionFromMainMenu(int optionEntered){
-
-        //only focusing on case 1 for now until architectural improvement.
-        switch (optionEntered){
-
-            case 1: System.out.println("Current Products Available");
-                options.clear();
-                options.addAll(showProducts());
-                menu.setMenuOptions(options);
-                int optionSelected = menu.displayMenu();
-                Product product = showProducts().get(optionSelected-1);
-                purchaseProduct(product);
-
-                break;
-
-            case 2: addProduct(new Product("Dummy",4.00,1));
-
-                System.out.println(ProductInventory.getProductInventory().get(ProductInventory.getProductInventory().size()-1).getName());break;
-
-            case 3:
-            case 4: System.exit(0);
-
-            default:System.out.println("default");
-        }
-    }
+//    private void processOptionFromMainMenu(int optionEntered){
+//
+//        //only focusing on case 1 for now until architectural improvement.
+//        switch (optionEntered){
+//
+//            case 1: System.out.println("Current Products Available");
+//                options.clear();
+//                options.addAll(showProducts());
+//                menu.setMenuOptions(options);
+//                int optionSelected = menu.displayMenu();
+//                Product product = showProducts().get(optionSelected-1);
+//                purchaseProduct(product);
+//
+//                break;
+//
+//            case 2: addProduct(new Product("Dummy",4.00,1));
+//
+//                System.out.println(ProductInventory.getProductInventory().get(ProductInventory.getProductInventory().size()-1).getName());break;
+//
+//            case 3:
+//            case 4: System.exit(0);
+//
+//            default:System.out.println("default");
+//        }
+//    }
 
 }
